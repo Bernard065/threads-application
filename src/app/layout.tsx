73f5b658343c294054ce4b048fe8e5bd2d1b869d
Bernard065 <bebenibernard@gmail.com>
2024-08-01 15:48:21 +0300

    Implement the navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
+import Header from "@/components/shared/Header";
+import LeftSideBar from "@/components/shared/LeftSideBar";
+import RightSideBar from "@/components/shared/RightSideBar";
+import Footer from "@/components/shared/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,8 +25,18 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={`${inter.className} bg-dark-1 head-text`}>
-          {children}
+        <body className={inter.className}>
+          <Header />
+
+          <main>
+            <LeftSideBar />
+            <section className="main-container">
+              <div className="w-full max-w-4xl">{children}</div>
+            </section>
+            <RightSideBar />
+          </main>
+
+          <Footer />
         </body>
       </html>
     </ClerkProvider>
